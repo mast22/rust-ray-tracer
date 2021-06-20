0c279163c56ae151190e0b26cbe17871b1b505be
Kozhenin Nick <kozhenin.nick@gmail.com>
2021-06-20 22:25:43 +0300

    Use Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 mod vector;
 
+const WIDTH: i32 = 256;
+const HEIGHT: i32 = 256;
+const COLORS: i32 = 256;
+
+fn print_header() {
+    println!("P3");
+    println!("{} {}", WIDTH, HEIGHT);
+    println!("{}", COLORS);
+}
+
 fn main() {
-    let width = 256;
-    let height = 256;
-    let colors = 255;
-    
-    let width_color_step = colors as f32 / width as f32;
-    let height_color_step = colors as f32 / height as f32;
+    let width_color_step = f64::from(COLORS) / f64::from(WIDTH);
+    let height_color_step = f64::from(COLORS) / f64::from(HEIGHT);
 
     // let point = vector::Point::new(&0.0, &0.0, &0.0);
 
-    println!("P3");
-    println!("{} {}", width, height);
-    println!("{}", colors);
+    print_header();
 
-    for h in 0..height {
-        for w in 0..width {
-            let r = h as f32 * height_color_step;
-            let g = w as f32 * width_color_step;
-            let b = 64;
-            println!("{} {} {}", r as i32, g as i32, b);
+    for h in 0..HEIGHT {
+        for w in 0..WIDTH {
+            let r = h as f64 / (WIDTH - 1) as f64 * width_color_step;
+            let g = w as f64 / (HEIGHT - 1) as f64 * height_color_step;
+            let c = vector::Vec3::new_with_coords(r, g, 0.25);
+            c.write_color();
         }
     }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,18 +1,55 @@
+use std::ops;
+
+
 #[derive(Debug)]
-pub struct Point {
+pub struct Vec3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
 
-impl Point {
-    pub fn new(&x: &f64, &y: &f64, &z: &f64) -> Point {
-        Point { x, y, z }
+
+impl Vec3 {
+    pub fn new_with_coords(x: f64, y: f64, z: f64) -> Self {
+        Vec3 { x, y, z }
+    }
+
+    pub fn new_center() -> Self {
+        Vec3 { x: 0.0, y: 0.0, z: 0.0 }
+    }
+
+    pub fn write_color(self) {
+        println!(
+            "{} {} {}",
+            (self.x * 259.99) as i32,
+            (self.y * 259.99) as i32,
+            (self.z * 259.99) as i32, 
+        )
     }
 }
 
-// В конце концов Вектор - это оберка над точкой
-pub type Vector = Point;
+// Utilities
 
+impl ops::Add for Vec3 {
+    type Output = Self;
 
-// Однако вектор требует дополнительного функционала для операций с ним
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
